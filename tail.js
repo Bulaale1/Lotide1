270cc334b3c9ea598a0cc2f249cf3e2b35747aa9
Yasin Hussein <bulaale116@gmail.com>
2024-01-08 14:58:54 -0700

    2ndCommit

--- tail.js
+++ tail.js
@@ -17,10 +17,21 @@ const tail = function(arr) {
   }
 };
 // Test Cases:
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-console.log(result);
-const result1 = tail([10,25,35,45]);
-console.log(result1);
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3);
+const array = ["Hello", "Lighthouse", "Labs"];
+const result = tail(array);
+assertEqual(array[1],result[0]);
+const array1 = [10,25,35,45];
+const result1 = tail(array1);
+assertEqual(array1[1],result1[0]);
+assertEqual(result.length, array.length - 1);
+assertEqual(result1.length, array1.length - 1);
+const oneElement = [8];
+const result2 = tail(oneElement);
+assertEqual(result2.length,0);
+const emptyArray = [];
+const result3 = tail(emptyArray);
+assertEqual(result3.length,0);
+
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words); // no need to capture the return value since we are not checking it
+// assertEqual(words.length, 3);
