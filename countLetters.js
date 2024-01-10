4ec869c8b6ae3cd0c287f428eea933339e661c7e
Yasin Hussein <bulaale116@gmail.com>
2024-01-10 10:04:07 -0700

    count the occurances of each letter

---
+++ countLetters.js
@@ -0,0 +1,29 @@
+/* eslint-disable linebreak-style */
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+const countLetters = function(sentence) {
+  let object = {};
+  let count = 1;
+  for (let letter of sentence) {
+    if (letter !== ' ') {
+      if (!object[letter]) {
+        object[letter] = count;
+      } else {
+        object[letter] += 1;
+      }
+    }
+  }
+  return object;
+};
+let sentence = "lighthouse in the house";
+console.log(sentence);
+let countedLetters = countLetters(sentence);
+assertEqual(countedLetters['e'],2);
+assertEqual(countedLetters['h'],4);
+assertEqual(countedLetters['i'],2);
