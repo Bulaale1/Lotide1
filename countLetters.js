d5b556878b83ae9b7dce844e3b4c32cad0069a62
Yasin Hussein <bulaale116@gmail.com>
2024-01-10 10:06:53 -0700

    counts the number of times each letter appears in a string

--- countLetters.js
+++ countLetters.js
@@ -9,11 +9,10 @@ const assertEqual = function(actual, expected) {
 };
 const countLetters = function(sentence) {
   let object = {};
-  let count = 1;
   for (let letter of sentence) {
     if (letter !== ' ') {
       if (!object[letter]) {
-        object[letter] = count;
+        object[letter] = 1;
       } else {
         object[letter] += 1;
       }
