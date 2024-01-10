48db05dce5ff08c39d5590aa54d18a4d7b072be0
Yasin Hussein <bulaale116@gmail.com>
2024-01-10 06:59:21 -0700

    return to middles

--- middle.js
+++ middle.js
@@ -22,17 +22,14 @@ const assertArraysEqual = function(actual, expected) {
 const middle = function(array) {
   if (array.length < 3) {
     return [];
-  }  else {
-    const mid = Math.floor(array.length / 2);
-    if (array.length % 2 === 0) {
-      
-      return [array[mid - 1], array[mid]];
-    } else {
-      return [array[mid]];
-    }
-
   }
-
+  const mid = Math.floor(array.length / 2);
+  if (array.length % 2 === 0) {
+      
+    return [array[mid - 1], array[mid]];
+  } else {
+    return [array[mid]];
+  }
 };
 
 // TEST CODE
