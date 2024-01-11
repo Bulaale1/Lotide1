707fd40de64ef6fb92285b3a9581426ac0f659a4
Yasin Hussein <bulaale116@gmail.com>
2024-01-11 14:00:17 -0700

    perfectlyMatchedObjectChecker

--- eqObjects.js
+++ eqObjects.js
@@ -12,26 +12,29 @@ const eqObjects = function(obj1, obj2) {
   if (obj1Keys.length !== obj2Keys.length) {
     return false;
   }
-  for (let key in  obj1) {
-    let property = obj1[key];
-    for (let anotherKey in obj2) {
-      let anotherProperty = obj2[anotherKey];
-      if (key === anotherKey) {
-        for (let i = 0; i < property.length; i++) {
-          if (property[i] !== anotherProperty[i]) {
-            return false;
-          }
+  for (let key in obj1) {
+    let property1 = obj1[key];
+    let property2 = obj2[key];
+    if (typeof(property1) === 'object' && typeof(property2) === 'object') {
+      for (let i = 0; i < property1.length; i++) {
+        if (property1[i] !== property2[i]) {
+          return false;
         }
       }
+    } else {
+      if (property1[key] !== property2[key]) {
+        return false;
+      }
+  
     }
   } return true;
 };
 const shirtObject = { color: "red", size: "medium" };
 const anotherShirtObject = { size: "medium", color: "red" };
-assertEqual(eqObjects(shirtObject , anotherShirtObject),true); // => true
 const multiColorShirtObject = { colors: ["red", "blue"], size: "medium" };
 const anotherMultiColorShirtObject = { size: "medium", colors: ["red", "blue"] };
 const longSleeveMultiColorShirtObject = { size: "medium", colors: ["red", "blue"], sleeveLength: "long" };
+assertEqual(eqObjects(shirtObject , anotherShirtObject),true); // => true
 assertEqual(eqObjects(longSleeveMultiColorShirtObject , anotherShirtObject),false); // => false
-assertEqual(eqObjects(multiColorShirtObject , anotherMultiColorShirtObject), false);//false
-assertEqual(eqObjects(multiColorShirtObject , longSleeveMultiColorShirtObject), true);// => false
+assertEqual(eqObjects(multiColorShirtObject , anotherMultiColorShirtObject), true);//should pass
+assertEqual(eqObjects(multiColorShirtObject , longSleeveMultiColorShirtObject), false);// => should pass
