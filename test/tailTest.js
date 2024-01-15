fc074578a8c6f7b0e64463214e3602c19afea6b7
Yasin Hussein <bulaale116@gmail.com>
2024-01-15 16:40:17 -0700

    middle and tail refactored with mocha and chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -6,10 +6,10 @@ const array = ["Hello", "Lighthouse", "Labs"];
 const array1 = [10,25,35,45];
 const oneElement = [8];
 describe("#tail", () => {
-  it(` ✅✅✅ passes for ["Hello", "Lighthouse", "Labs"]`, () => {
+  it(`returns ["Lighthouse", "Labs"] when input is ["Hello", "Lighthouse", "Labs"]`, () => {
     assert.deepEqual(tail(array), ["Lighthouse", "Labs"]);
   });
-  it(`✅✅✅ Passes for [10,25,35,45]`, () => {
+  it(`return [25,35,45] when input array is [10,25,35,45]`, () => {
     assert.deepEqual(tail(array1), [25,35,45]);
   });
   it(`returns an empty array when the input array has only one element`, () => {
