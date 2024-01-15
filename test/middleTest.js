cef71334dd4311226f194ae7bed7e6807e4e3024
Yasin Hussein <bulaale116@gmail.com>
2024-01-15 08:06:20 -0700

    middle refactored function with commented cases

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,7 +1,16 @@
 /* eslint-disable linebreak-style */
 const assertArraysEqual = require('../assertArraysEqual');
 const middle = require('../middle');
+
 assertArraysEqual(middle([1]),[]);
+// Test case: An array with two elements should return an empty array
 assertArraysEqual(middle([1, 2]),[]);
+// rest of your code
+// Test case: An array with one element should return an empty array since there is no middle element
+assertArraysEqual(middle([1]),[]);
+// Test case: An array with two element should return an empty array since there is no middle element
+assertArraysEqual(middle([1, 2]),[]);
+// Test case: An even array with more than 2 element should return the two middle element
 assertArraysEqual(middle([1, 2, 3, 4, 5, 6]),[3,4]);
+// Test case: An odd array with more than 2 element should return the  middle element
 assertArraysEqual(middle([1, 2, 3, 4]),[3,4]);
