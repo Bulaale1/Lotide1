6faf1d3479997c8b2526ca6773fe42c4009b1f62
Yasin Hussein <bulaale116@gmail.com>
2024-01-15 07:54:10 -0700

    middle function refactored

--- middle.js
+++ middle.js
@@ -1,23 +1,6 @@
 /* eslint-disable linebreak-style */
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual,expected)) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    
-    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+// const eqArrays = function(arr1, arr2) {
+//   if (arr1.length !== arr2.length) {
 const middle = function(array) {
   if (array.length < 3) {
     return [];
@@ -31,9 +14,4 @@ const middle = function(array) {
   }
 };
 
-// TEST CODE
-// ...
-assertArraysEqual(middle([1]),[]);
-assertArraysEqual(middle([1, 2]),[]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]),[3,4]);
-assertArraysEqual(middle([1, 2, 3, 4]),[3,4]);
+module.exports = middle;
