e5514af1eb1f99524089825e37ceee50a0c2a219
Yasin Hussein <bulaale116@gmail.com>
2024-01-15 16:27:58 -0700

    middle and tail refactored with mocha and chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -3,10 +3,10 @@ const middle = require('../middle');
 const assert = require('chai').assert;
 
 describe("#middle", () => {
-  it(` should return [] for [1] `, () => {
+  it(`returns an empty array when the input array has only one element. `, () => {
     assert.deepEqual(middle([1]),[]);
   });
-  it(`should teturn [] for [1, 2])`, () => {
+  it(`should return [] for [1, 2])`, () => {
     assert.deepEqual(middle([1, 2]),[]);
   });
   it(` it should return true for [1, 2, 3, 4, 5, 6] `, () => {
--- test/tailTest.js
+++ test/tailTest.js
@@ -3,20 +3,16 @@ const tail = require('../tail');
 const assert = require('chai').assert;
 // Test Cases:
 const array = ["Hello", "Lighthouse", "Labs"];
-const result = tail(array);
 const array1 = [10,25,35,45];
-const result1 = tail(array1);
 const oneElement = [8];
-const result2 = tail(oneElement);
-
 describe("#tail", () => {
   it(` ✅✅✅ passes for ["Hello", "Lighthouse", "Labs"]`, () => {
-    assert.deepEqual(tail(array),result);
+    assert.deepEqual(tail(array), ["Lighthouse", "Labs"]);
   });
   it(`✅✅✅ Passes for [10,25,35,45]`, () => {
-    assert.deepEqual(tail(array1),result1);
+    assert.deepEqual(tail(array1), [25,35,45]);
   });
-  it(`⛔⛔⛔ return false for [8]`, () => {
-    assert.deepEqual(tail(oneElement),result2);
+  it(`returns an empty array when the input array has only one element`, () => {
+    assert.deepEqual(tail(oneElement)[8]);
   });
 });
