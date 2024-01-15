0aff40f14c7d2b1ab2edff8ea6c67337829ce341
Yasin Hussein <bulaale116@gmail.com>
2024-01-15 14:51:37 -0700

    assertionWithMocha

--- test/headTest.js
+++ test/headTest.js
@@ -1,8 +1,12 @@
 /* eslint-disable linebreak-style */
+const assert = require('chai').assert;
+const head   = require('../head');
 
-const head = require('../head');
-const assertEqual = require('../assertEqual');
-assertEqual(head([4,5,6]),4);
-assertEqual(head([4,5,6]),4);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]),undefined);
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+});
