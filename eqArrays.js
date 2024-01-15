b4dfc317aee38324f6c01c7a443386a1c5a25bad
Yasin Hussein <bulaale116@gmail.com>
2024-01-15 07:44:38 -0700

    modularized some function

--- eqArrays.js
+++ eqArrays.js
@@ -1,12 +1,5 @@
 /* eslint-disable linebreak-style */
-// const assertEqual = function(actual, expected) {
-//   if (actual === expected) {
-//     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-//   } else {
-    
-//     console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-//   }
-// };
+
 const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
